149b71d4e21429b137b48281c53da128e3842b62
Y0L042 <vlamfletcher@gmail.com>
2024-01-03 18:23:54 +0200

    feat(state_class.gd): Add state class for state machines. Status: beta
    Workstation: JJDesktop

---
+++ GScripts/StateMachineScripts/state_class.gd
@@ -0,0 +1,30 @@
+class_name StateClass
+extends Node
+
+var node_parent: Variant
+var state_parent: StateClass
+var current_state: StateClass
+
+func _init(i_node_parent: Variant, i_state_parent: StateClass) -> void:
+	node_parent = i_node_parent
+	state_parent = i_state_parent
+
+func enter() -> void:
+	pass
+
+func custom_process(_delta: float) -> void:
+	if current_state: current_state.custom_process(_delta)
+
+func custom_physics_process(_delta: float) -> void:
+	if current_state: current_state.custom_physics_process(_delta)
+
+func custom_unhandled_input(_event: InputEvent) -> void:
+	if current_state: current_state.custom_unhandled_input(_event)
+
+func exit() -> void:
+	pass
+
+func change_state(i_new_state: StateClass) -> void:
+	if current_state: current_state.exit();
+	current_state = i_new_state
+	if current_state: current_state.enter()
