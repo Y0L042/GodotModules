e6edb7d590fdb881599b85deb2e0357202e5d615
Y0L042 <vlamfletcher@gmail.com>
2024-01-04 13:35:39 +0200

    feat(editor_only_nodes): Add an editor_only_node that despawns when game is running. Feature Status: beta
    This is useful for placing props and stuff that you only want to appear in the editor.
    
    Prototype
    
    Workstation: JJDesktop

---
+++ GDebugTools/EditorOnlyNodes/editor_only_nodes.gd
@@ -0,0 +1,8 @@
+@tool
+@icon("res://GodotModules/GDebugTools/EditorOnlyNodes/icons/icons8-spanner-100.png")
+class_name EditorOnlyNodes
+extends Node3D
+
+func _ready() -> void:
+	if !Engine.is_editor_hint():
+		queue_free()
