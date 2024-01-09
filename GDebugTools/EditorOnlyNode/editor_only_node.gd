e8fa15e066c25f4dfd1c0f3ece6fcf532a7b659a
Y0L042 <vlamfletcher@gmail.com>
2024-01-09 18:43:37 +0200

    feat(editor_only_node): Node is now toggleable.
    Workstation: JJDesktop

--- GDebugTools/EditorOnlyNode/editor_only_node.gd
+++ GDebugTools/EditorOnlyNode/editor_only_node.gd
@@ -3,6 +3,10 @@
 class_name EditorOnlyNodes
 extends Node3D
 
+@export var enabled: bool = true
+
 func _ready() -> void:
-	if !Engine.is_editor_hint():
+	if !enabled:
+		push_warning("EditorOnlyNode is disabled: ", self)
+	if !Engine.is_editor_hint() and enabled:
 		queue_free()
